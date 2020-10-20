224678f63b986eae8c32c380caa5e2e7aaeac97b
webcdh <764157913@qq.com>
2020-10-20 17:34:57 +0800

    feat: 地图控件

--- src/plugins/ol.js
+++ src/plugins/ol.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import Map from "ol/Map";   // Map地图层
-import View from "ol/View"; // Map可视视图层
+import Map from "ol/Map"   // Map地图层
+import View from "ol/View" // Map可视视图层
 import * as Layer from "ol/layer" // 虚基类图层，可视化表达（矢量地图数据、栅格地图数据）
 import * as Source from "ol/source" // 
 import * as Control from "ol/control" // 地图控件基类
+import * as Coordinate from 'ol/coordinate' // 
+import * as Tilegrid from "ol/tilegrid" // 
+import * as Proj from "ol/proj" // 
 
 // 地图Css样式加载
 import "ol/ol.css";
@@ -14,6 +16,9 @@ let ol = {
   Layer: Layer,
   Source: Source,
   Control: Control,
+  Coordinate: Coordinate,
+  Tilegrid: Tilegrid,
+  Proj: Proj,
 }
 
 export default ol
